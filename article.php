522f7b2a311240508830636d6d89bbf9ad0233f2
zinmyatnwe <zinmyatnwe4@gmail.com>
2023-01-26 11:01:34 +0630

    change code

--- article.php
+++ article.php
@@ -1,36 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-    
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"
-        />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-        <link rel="stylesheet" href="./css/style.css" />
-        <script src="./js/jquery.min.js"></script>
-        <link rel="icon" href="./img/icon.png" />
-        <title>Food Delivery Landing Page</title>
-    </head>
-    <nav class="navbar navbar-expand-md navbar-light fixed-top bg-white"> <a class="navbar-brand" href="./index.php"><img src="./img/logo.png"></a>
-        <button
-        class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
-        aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active"> <a class="nav-link" href="./index.php">Welcome<span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="./index.php#menu">Today's Menu</a>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="./login.php">Split Page</a>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="./article.php">Article</a>
-                    </li>
-                </ul> <a href="https://github.com/wowthemesnet/template-fooddelivery-bootstrap-html/archive/master.zip"
-                class="btn btn-outline-dark">Download Template</a>
-            </div>
-    </nav>
+<?php
+include_once 'layouts/header.php';
+
+?>
     
     <body>
         <div class="maincontent">
@@ -64,16 +35,6 @@
                 </div>
             </div>
         </div>
-        <footer class="block footer1 footer text-center">
-            <p>&copy; <a href="https://bootstrapstarter.com/bootstrap-templates/template-fooddelivery-bootstrap-html/">"Food Delivery HTML Template"</a> by
-                WowThemes.net</p>
-        </footer>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-        <script>
-            AOS.init();
-        </script>
-        <script src="js/custom-general.js"></script>
-    </body>
-
-</html>
+        <?php
+    include_once 'layouts/footer.php';
+        ?>
