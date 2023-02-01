12025e47266d4437c9f9338ddefdc2f279716ef9
Arkar Nyein <yokesoe20678@gmail.com>
2023-02-01 22:34:49 +0630

    update order.php

---
+++ admin/model/order_model.php
@@ -0,0 +1,21 @@
+<?php
+include_once __DIR__."/../includes/db.php";
+
+
+class OrderModel{
+    private $pdo;
+    public function get_order_info(){
+        $this->pdo=DataBase::connect();
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        $query="select orders.*,users_info.name as cus_name
+        FROM orders JOIN users_info
+        WHERE users_info.id=orders.user_info_id";
+        $statement=$this->pdo->prepare($query);
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_ASSOC); 
+    }
+    
+}
+    
+
+?>
